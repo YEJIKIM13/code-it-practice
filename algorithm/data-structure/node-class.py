0decbb025c2079062685ccc17f5b115dd1cb88fa
YEJIKIM13 <lerry597@gmail.com>
2021-02-15 14:49:51 +0900

    링크드 리스트 접근 연산

--- algorithm/data-structure/node-class.py
+++ algorithm/data-structure/node-class.py
@@ -9,33 +9,21 @@ class Node:
         self.data = data # 노드가 저장하는 데이터
         self.next = None # 다음 노드에 대한 레퍼런스, 일단 비워둠
 
-# 데이ㅓ 2, 3, 5, 7, 11을 담는 노드들 생성, 아직은 서로 연결안됨
-head_node = Node(2) # 2가 data 파라미터로 들어가고 self.data 속성에 data, 즉 2가 저장됨
-node_1 = Node(3)
-node_2 = Node(5)
-node_3 = Node(7)
-tail_node = Node(11)
-
-# 노드들을 연결
-head_node.next = node_1
-node_1.next = node_2
-node_2.next = node_3
-node_3.next = tail_node
-
-# 노드 순서대로 출력
-iterator = head_node # iterate - 반복, 반복문으로 리스트를 돌 때 도움을 주는 역할을 하는 값
-
-while iterator is not None:
-    print(iterator.data)
-    iterator = iterator.next # 갱신
-
-
 class LinkedList:
     """링크드 리스트 클래스"""
     def __init__(self):
         # 인스턴스 변수들의 초기값 설정, 여기선 속성 두 가지
         self.head = None
         self.tail = None
+
+    def find_node_at(self, index):
+        """링크드 리스트 접근 연산 메소드"""
+        iterator = self.head
+
+        for _ in range(index):
+            iterator = iterator.next
+
+        return iterator
 
     # 링크드 리스트 맨 끝에 노드 정보를 추가하는 메소드 작성
     # data - 우리가 추가하려는 데이터
@@ -50,6 +38,21 @@ class LinkedList:
             self.tail.next = new_node
             self.tail = new_node
 
+    def __str__(self):
+        """링크드 리스트를 문자열로 표현해서 리턴하는 메소드"""
+        res_str = "|"
+
+        # 링크드  리스트 안에 모든 노드를 돌기 위한 변수. 일단 가장 앞 노드로 정의한다.
+        iterator = self.head
+
+        # 링크드  리스트 끝까지 돈다
+        while iterator is not None:
+            # 각 노드의 데이터를 리턴하는 문자열에 더해준다
+            res_str += f" {iterator.data} |"
+            iterator = iterator.next  # 다음 노드로 넘어간다
+
+        return res_str
+
 # 링크드 리스트에 데이터 추가
 my_list = LinkedList()
 my_list.append(2)
@@ -58,9 +61,11 @@ my_list.append(5)
 my_list.append(7)
 my_list.append(11)
 
-# 링크드 리스트 출력
-iterator = my_list.head # head_node 넣으면 돼서 my_list의 head속성 넣으면 됨
+# 링크드 리스트 노드에 접근 (데이터 가지고 오기)
+print(my_list.find_node_at(3).data)
 
-while iterator is not None:
-    print(iterator.data)
-    iterator = iterator.next
+# 링크드 리스트 노드에 접근 (데이터 바꾸기)
+my_list.find_node_at(2).data = 13
+
+# 전체 링크드 리스트 출력
+print(my_list)
